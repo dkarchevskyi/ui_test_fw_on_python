470e1d18bf218bc106b270ce15b2142a168c1e8e
Denys K <denys.karchevskyi@gmail.com>
2024-02-08 10:05:22 -0500

    fix

--- locators/locators.py
+++ locators/locators.py
@@ -2,21 +2,18 @@ from random import randint
 
 from selenium.webdriver.common.by import By
 
+
 class PageLocators:
-
-    #form fields
-
+    # form fields
 
     FIRST_NAME = (By.CSS_SELECTOR, '#firstName')
     LAST_NAME = (By.CSS_SELECTOR, '#lastName')
     EMAIL = (By.CSS_SELECTOR, '#userEmail')
-    GENDER = (By.CSS_SELECTOR, f"label[for='gender-radio-{randint(1,3)}']")
+    GENDER = (By.CSS_SELECTOR, f"label[for='gender-radio-{randint(1, 3)}']")
     MOBILE = (By.CSS_SELECTOR, '#userNumber')
     SUBJECT = (By.CSS_SELECTOR, '#subjectsInput')
-    HOBBIES = (By.CSS_SELECTOR, f'label[for="hobbies-checkbox-{randint(1,3)}"]')
+    HOBBIES = (By.CSS_SELECTOR, f'label[for="hobbies-checkbox-{randint(1, 3)}"]')
     FILE_INPUT = (By.CSS_SELECTOR, '#uploadPicture')
     CURRENT_ADDRESS = (By.CSS_SELECTOR, 'textarea[id="currentAddress"]')
     PERMANENT_ADDRESS = (By.CSS_SELECTOR, 'textarea[id="permanentAddress"]')
     SUBMIT = (By.ID, "submit")
-
-
