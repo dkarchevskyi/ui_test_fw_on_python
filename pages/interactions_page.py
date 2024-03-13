c686940c70730836a34e4ee5668f2c2d1e43be68
Denys K <denys.karchevskyi@gmail.com>
2024-03-13 14:40:05 -0400

    Sortable tests were added

--- pages/interactions_page.py
+++ pages/interactions_page.py
@@ -12,5 +12,26 @@ from selenium.webdriver.common.keys import Keys
 class SortablePage(BasePage):
     locators = SortablePageLocators
 
-    def check_sortable(self):
-        pass
+    def get_sortable_items(self, elements):
+        item_list = self.elements_are_visible(elements)
+        return [item.text for item in item_list]
+
+    def change_list_order(self):
+        self.element_is_visible(self.locators.LIST_TAB).click()
+        order_before = self.get_sortable_items(self.locators.LIST_ITEM)
+        item_list = random.sample(self.elements_are_visible(self.locators.LIST_ITEM), k=2)
+        item_what = item_list[0]
+        item_where = item_list[1]
+        self.action_drug_and_drop_to_element(item_what, item_where)
+        order_after = self.get_sortable_items(self.locators.LIST_ITEM)
+        return order_before, order_after
+
+    def change_grid_order(self):
+        self.element_is_visible(self.locators.GRID_TAB).click()
+        order_before = self.get_sortable_items(self.locators.GRID_ITEM)
+        item_list = random.sample(self.elements_are_visible(self.locators.GRID_ITEM), k=2)
+        item_what = item_list[0]
+        item_where = item_list[1]
+        self.action_drug_and_drop_to_element(item_what, item_where)
+        order_after = self.get_sortable_items(self.locators.GRID_ITEM)
+        return order_before, order_after
--- tests/interactions_test.py
+++ tests/interactions_test.py
@@ -2,13 +2,16 @@ import pytest
 import time
 
 import generator.generator
-from pages.form_page import FormPage
 from pages.interactions_page import SortablePage
 
 
 class TestSortablePage:
 
     @pytest.mark.sortable
-    def test_form(self, driver):
+    def test_sortable(self, driver):
         sortable_page = SortablePage(driver, 'https://demoqa.com/sortable')
         sortable_page.open()
+        list_before, list_after = sortable_page.change_list_order()
+        assert list_before != list_after, "Items were not moved"
+        grid_before, grid_after = sortable_page.change_grid_order()
+        assert grid_before != grid_after, "Items were not moved"
